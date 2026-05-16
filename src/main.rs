89044b178be5f28b9f391f1b14440b2403a2c21a
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-05-16 21:43:18 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::env::current_dir;
 use std::io::{self, Write};
 use std::str::FromStr;
+use walkdir::WalkDir;
 
 fn main() {
     loop {
@@ -35,9 +37,34 @@ fn echo_command(args: &str){
 fn type_command(args: &str) {
     match BuiltinCommand::from_str(args) {
         Ok(_) => println!("{} is a shell builtin", args),
-        Err(_) => println!("{}: not found", args)
+        Err(_) => search_path_directories(args),
     }
 }
+
+fn search_path_directories(args: &str) {
+    let current_directory = current_dir().unwrap_or_default();
+
+    for entry in WalkDir::new(current_directory) {
+        let entry = entry.unwrap();
+        if entry.file_type().is_file() && entry.file_name() == args {
+            println!("{} is {}", args, entry.path().display());
+            return;
+        }
+    }
+
+    println!("{}: not found", args)
+}
+
+/*
+When type receives a command input, your shell must follow these steps:
+Check if the command is a builtin command (like exit or echo). If it is, report it as a builtin (<command> is a shell builtin) and stop.
+If the command is not a builtin, your shell must go through every directory in PATH. For each directory:
+    Check if a file with the command name exists.
+    Check if the file has execute permissions.
+    If the file exists and has execute permissions, print <command> is <full_path> and stop.
+    If the file exists but lacks execute permissions, skip it and continue to the next directory.
+If no executable is found in any directory, print <command>: not found.
+*/
 
 enum BuiltinCommand {
     Exit,
